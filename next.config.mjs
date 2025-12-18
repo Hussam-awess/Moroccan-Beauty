d050b414442178eddb3087738349fe328667545a
Hussam Awess <hussamawess@gmail.com>
2025-12-19 00:40:22 +0300

    Remove static export to allow dynamic routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
 }
 
-export default nextConfig
+module.exports = nextConfig
+
